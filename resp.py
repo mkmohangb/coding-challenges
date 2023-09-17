280d75af9b6ce597e1bf4ad5ab974c2242634076
Manoj Mohan <haimanojkumar@gmail.com>
2023-09-17 10:14:27 +0530

    process PING command

--- resp.py
+++ resp.py
@@ -1,10 +1,11 @@
-
+import socket
 # Redis Serialization Protocol - RESP
 # https://redis.io/docs/reference/protocol-spec/
 # - Simple to implement, fast to parse, human readable
 # TCP - 6379, or Unix sockets
 
-CRLF = 2
+CRLF = "\r\n"
+LEN_CRLF = len(CRLF)
 
 
 def parse_value(cmd):
@@ -13,7 +14,7 @@ def parse_value(cmd):
     while cmd[i] != '\r':
         parts += cmd[i]
         i += 1
-    return ("".join(parts), i+CRLF)
+    return ("".join(parts), i+LEN_CRLF)
 
 
 def parse_command(cmd):
@@ -36,7 +37,7 @@ def parse_command(cmd):
         print(length)
         value = cmd[offset:offset+length]
         values.append(value)
-        offset += length + CRLF
+        offset += length + LEN_CRLF
         print(value)
         print(offset)
     elif cmd_type == '+' or cmd_type == ':' or cmd_type == '-':
@@ -46,10 +47,51 @@ def parse_command(cmd):
     return (values, offset)
 
 
+def get_simple_string_reply(text):
+    return "+" + text + CRLF
+
+
+def get_bulk_string_reply(text):
+    return "$" + str(len(text)) + CRLF + text + CRLF
+
+
+def construct_reply(cmds):
+    resp = ""
+    if cmds[0] == "PING":
+        if len(cmds) == 1:
+            resp = get_simple_string_reply("PONG")
+        else:
+            resp = get_bulk_string_reply(cmds[1])
+    return resp
+
+
+def listen():
+    HOST = '127.0.0.1'
+    PORT = 50007              # Arbitrary non-privileged port
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                print(data, type(data))
+                if not data:
+                    break
+                cmds, _ = parse_command(str(data, encoding='utf-8'))
+                print('commands: ', cmds)
+                reply = construct_reply(cmds)
+                if reply != "":
+                    data = bytes(reply, 'utf-8')
+                conn.sendall(data)
+
+
 if __name__ == "__main__":
-    parse_command("+OK\r\n")
-    parse_command("-ERR unknown command 'asdf'\r\n")
-    parse_command(":1000\r\n")
-    parse_command("*2\r\n$4\r\necho\r\n$5\r\nhello\r\n")
-    parse_command("*3\r\n:1\r\n:2\r\n:3\r\n")
-    parse_command("$5\r\nworld\r\n")
+    # parse_command("+OK\r\n")
+    # parse_command("-ERR unknown command 'asdf'\r\n")
+    # parse_command(":1000\r\n")
+    # parse_command("*2\r\n$4\r\necho\r\n$5\r\nhello\r\n")
+    # parse_command("*3\r\n:1\r\n:2\r\n:3\r\n")
+    # parse_command("$5\r\nworld\r\n")
+    listen()
