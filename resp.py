a9a2c807e50e2e3b0f368dcc91e0384b81d751b6
Manoj Mohan <haimanojkumar@gmail.com>
2023-10-03 11:12:53 +0530

    using asyncio for concurrent clients

--- resp.py
+++ resp.py
@@ -1,4 +1,4 @@
-import socket
+import asyncio
 import sys
 import threading
 # Redis Serialization Protocol - RESP
@@ -28,23 +28,23 @@ def parse_command(cmd):
     offset += pos
     if cmd_type == '*':
         count = int(value)
-        print(count, pos)
+        #print(count, pos)
         for _ in range(count):
             value, rel_offset = parse_command(cmd[offset:])
             values.append(value[0])
             offset += rel_offset
-        print(values)
+        #print(values)
     elif cmd_type == "$":
         length = int(value)
-        print(length)
+        #print(length)
         value = cmd[offset:offset+length]
         values.append(value)
         offset += length + LEN_CRLF
-        print(value)
-        print(offset)
+        #print(value)
+        #print(offset)
     elif cmd_type == '+' or cmd_type == ':' or cmd_type == '-':
         values.append(value)
-        print(value)
+        #print(value)
 
     return (values, offset)
 
@@ -87,37 +87,29 @@ def construct_reply(cmds, lock):
     return resp
 
 
-def handle_client(conn, addr, lock):
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            print(data, type(data))
-            if not data:
-                print("Exiting client")
-                break
-            cmds, _ = parse_command(str(data, encoding='utf-8'))
-            print('commands: ', cmds)
-            reply = construct_reply(cmds, lock)
-            if reply != "":
-                data = bytes(reply, 'utf-8')
-            else:
-                print("Empty reply")
-            conn.sendall(data)
+async def handle_client(reader, writer, lock):
+    while True:
+        data = await reader.read(100)
+        message = data.decode()
+        cmds, _ = parse_command(message)
+        # print('commands: ', cmds)
+        reply = construct_reply(cmds, lock)
+        if reply != "":
+            data = bytes(reply, 'utf-8')
+        else:
+            print("Empty reply")
+        writer.write(data)
+        await writer.drain()
 
 
-def listen():
+async def listen():
     HOST = '127.0.0.1'
     PORT = int(sys.argv[1])              # Arbitrary non-privileged port
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(1)
-        while True:
-            conn, addr = s.accept()
-            lock = threading.Lock()
-            thread = threading.Thread(target=handle_client, args=(conn,addr,lock))
-            thread.start()
-            print("number of threads: ", threading.active_count() - 1)
+    lock = threading.Lock()
+    server = await asyncio.start_server(
+            lambda r, w: handle_client(r, w, lock), HOST, PORT)
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
@@ -127,4 +119,4 @@ if __name__ == "__main__":
     # parse_command("*2\r\n$4\r\necho\r\n$5\r\nhello\r\n")
     # parse_command("*3\r\n:1\r\n:2\r\n:3\r\n")
     # parse_command("$5\r\nworld\r\n")
-    listen()
+    asyncio.run(listen())
